14cf33b67ceb1543f68e37ec88aec5a552a64e8d
Djordje Knezevic <starwarrior@mycity.rs>
2021-11-25 23:41:27 +0100

    Add custom MeetupDetail component and its styling

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+    return (
+      <MeetupDetail 
+           image='https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Шид_%28главна_улица%29_-_Šid_%28main_street%29.JPG/1280px-Шид_%28главна_улица%29_-_Šid_%28main_street%29.JPG'
+           title='First Meetup'
+           address='Three Dogs NN, 22240 Sid'
+           description='This is a first meetup'
+      />
+    );
+};
+
+export default MeetupDetails;
