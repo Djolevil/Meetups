a423b2cf3f6b785782b8571f4cbde4804e7266f0
Djordje Knezevic <starwarrior@mycity.rs>
2021-11-27 19:43:18 +0100

    Reverse back to SSG and use it with params for meetup detail page

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,23 @@ function MeetupDetails() {
     );
 };
 
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Шид_%28главна_улица%29_-_Šid_%28main_street%29.JPG/1280px-Шид_%28главна_улица%29_-_Šid_%28main_street%29.JPG',
+        id: meetupId,
+        title: 'First Meetup',
+        address: 'Three Dogs NN, 22240 Sid',
+        description: 'This is a first meetup',
+      },
+    },
+  };
+};
+
 export default MeetupDetails;
