7586ecd5d9674572eebf269f13318abec344471c
Djordje Knezevic <starwarrior@mycity.rs>
2021-11-28 20:53:47 +0100

    Implement MongoDB for backend

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,13 +1,23 @@
+import { MongoClient } from 'mongodb';
+
 // /api/new-meetup
 // POST /api/mew-meetup
 
-function handler(req, res) {
+async function handler(req, res) {
     if (req.method === 'POST') {
        const data = req.body;
 
-       const { title, image, address, description } = data;
+       const client = await MongoClient.connect('mongodb+srv://Stevizza:Kurchubic@cluster0.xtgqi.mongodb.net/meetups?retryWrites=true&w=majority');
+       const db = client.db();
 
-       
+       const meetupsCollection = db.collection('meetups');
+       const result = awaitmeetupsCollection.insertOne(data);
+
+       console.log(result);
+
+       client.close();
+
+       res.status(201).json({ message: 'Meetup inserted!' });
     }
 };
 
